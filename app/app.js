efddfaec2a89edea6af45d1741966607872b95f2
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-01-26 12:04:09 +0200

    update

---
+++ app/app.js
@@ -0,0 +1,79 @@
+'use strict'
+
+//global var
+
+var form=document.getElementById('formId');
+var table=document.getElementById('tableId');
+
+
+var arrayOfCars=[];
+
+function Cars(name,cat,modle){
+
+    this.name=name;
+    this.cat=cat;
+    this.modle=modle;
+
+    arrayOfCars.push(this);
+}
+
+
+Cars.prototype.renderCars=function(){
+
+    
+    var tr=document.createElement('tr');
+    
+    var td=document.createElement('td');
+    var img=document.createElement('img');
+
+    img.setAttribute('src',this.cat)
+    tr.appendChild(td);
+    td.textContent='Car Name: '+this.name+'   Model Year: '+this.modle;
+
+
+    
+    td.appendChild(img);
+    table.appendChild(tr);
+
+}
+
+//functions
+
+
+function checkIfEmpty(){
+
+    if(localStorage.getItem('carsData'))
+    {
+        var newObj=JSON.parse(localStorage.getItem('carsData'))
+
+        for (var index = 0; index < newObj.length; index++) {
+            var newCar=new Cars(newObj[index].name,newObj[index].cat,newObj[index].modle);
+
+            console.log(newCar);
+            newCar.renderCars();
+            
+        }
+        
+    }
+}
+
+
+
+function submitForm(event){
+    event.preventDefault();
+
+    var carName=event.target.carName.value;
+    var category=event.target.category.value;
+    var modle=event.target.modle.value;
+
+    var newCar=new Cars(carName,category,modle);
+
+    newCar.renderCars();
+    localStorage.setItem('carsData',JSON.stringify(arrayOfCars))
+
+
+}
+
+
+checkIfEmpty();
+addEventListener('submit',submitForm);
